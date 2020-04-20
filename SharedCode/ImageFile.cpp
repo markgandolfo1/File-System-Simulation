0a0e6e75651494cdca087c3e38ee2a89790ca1c7
juliann <juliann@RDSH01-FL19>
2020-04-19 23:56:04 -0400

    started 17

--- SharedCode/ImageFile.cpp
+++ SharedCode/ImageFile.cpp
@@ -1,1 +1,39 @@
 // definition of ImageFile class here
+#include "ImageFile.h"
+
+unsigned ImageFile::getSize() {
+	return contents.size();
+}
+string ImageFile::getName() {
+	return name;
+}
+int ImageFile::write(std::vector<char> i) {
+	bool fail = false;
+	int x = i.size();
+	int  s = i[x-1];
+	s = s - 48;
+	if (s * s != x - 1) {
+		fail = true;
+	}
+	size = s;
+	for (int k = 0; k < x - 1; ++k) {
+		contents.push_back(i[k]);
+		if (i[k] != 'X' || i[k] != ' ') {
+			fail = true;
+		}
+	}
+	if (fail == false) {
+		return 0;
+	}
+	else {
+		contents.clear();
+		size = 0;
+		return 1;
+	}
+
+
+
+}
+int ImageFile::append(std::vector<char> i) {
+	return 2;
+}
--- SharedCode/ImageFile.h
+++ SharedCode/ImageFile.h
@@ -1,2 +1,18 @@
 #pragma once
 // Image file class declaration here
+#include "AbstractFile.h"
+class ImageFile : public AbstractFile {
+public:
+	ImageFile(string s) {
+		name = s;
+		size = 0;
+	}
+	virtual unsigned getSize();
+	virtual string getName();
+	virtual int write(std::vector<char> i);
+	virtual  int append(std::vector<char> i);
+private:
+	string name;
+	vector<char> contents;
+	char size;
+};
